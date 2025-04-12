1c40243a04e2a575f05748a6317a2aaaf2aec1b2
JjuukoHenry12 <juukohenry23@gmail.com>
2025-04-12 23:38:50 +0300

    update

--- src/main/java/com/systemproject/systemproject/controller/ProductController.java
+++ src/main/java/com/systemproject/systemproject/controller/ProductController.java
@@ -1,19 +1,50 @@
 package com.systemproject.systemproject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.systemproject.systemproject.model.Product;
 import com.systemproject.systemproject.service.ProductService;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class ProductController {
 
     @Autowired
     ProductService service;
-
+     
+    @GetMapping("/products")
     public List<Product>getProduct(){
         return service.getProduct();
     }
+
+    @GetMapping("/product/{productId}")
+    public Product getProductById(@PathVariable("productId") int productId){
+        return service.getProductById(productId);
+    }
+
+    @PostMapping("/addproduct")
+    public void addProduct(Product product){
+        service.addProduct(product);
+    }
     
+
+    //update product
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product product){
+        service.updateProduct(product);
+    }
+       
+     // delete products
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProduct(@PathVariable("prodId") int prodId){
+        service.deleteProduct(prodId);
+    }
 }
--- src/main/java/com/systemproject/systemproject/service/ProductService.java
+++ src/main/java/com/systemproject/systemproject/service/ProductService.java
@@ -10,11 +10,50 @@ public class ProductService {
 
     List <Product> products = Arrays.asList(
         new Product(012,"Nike Shoes",145000),
-        new Product(034,"water proof bag",50000)
+        new Product(034,"water proof bag",50000),
+        new Product(056,"Apple Watch",200000),
+        new Product(002,"Samsung TV",800000),
+        new Product(003,"Sony Headphones",30000)
+
     );
     
     public List<Product>getProduct(){
         return products;
 
+    }
+
+    public Product getProductById(int id) {
+        return products.stream()
+        .filter(product -> product.getProdId() == id)
+        .findFirst()
+        .orElse(null);
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+
+    }
+
+    public void updateProduct(Product prod){
+
+        int index =0;
+
+        for(int i =0 ; i<products.size();i++){
+            if(products.get(i).getProdId() == prod.getProdId()){
+                index=i;
+                products.set(index, prod);
+            }
+        }
+    }
+
+    public void deleteProduct(int prodId){
+        int index =0;
+        
+        for(int i =0 ; i<products.size();i++){
+            if(products.get(i).getProdId() == prodId){
+                index=i;
+                products.remove(index);
+            }
+        }
     }
 }
